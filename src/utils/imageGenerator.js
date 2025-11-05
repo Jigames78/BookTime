b82af4c1264de048a7ee9671eb8e20424975c7d6
Jigames78 <jimmy.fernandes1503@gmail.com>
2025-11-05 15:49:16 +0100

    Delete src directory

--- src/utils/imageGenerator.js
+++
@@ -1,4 +0,0 @@
-export const getCoverUrl = (title) => {
-  const seed = title.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
-  return `https://picsum.photos/seed/${seed}/400/600`;
-};
